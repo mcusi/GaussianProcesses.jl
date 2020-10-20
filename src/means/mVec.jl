fc7afdb2bc9d150e34cb23d5dff76d41b707fd79
Maddie Cusimano <maddiecusimano@Maddies-MacBook-Pro.local>
2020-10-20 13:41:29 -0400

    fix params

--- src/means/mVec.jl
+++ src/means/mVec.jl
@@ -52,6 +52,6 @@ end
 function grad_mean(mVec::MeanVec, x::AbstractVector)
     println("Grad_mean x: ", x)
     println("Grad_mean mVec: ", mVec)
-    dM_theta = ones(1)
+    dM_theta = ones(length(mVec.β))
     return dM_theta
 end
