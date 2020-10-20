8b00657bdf4170db6e3bb6d3a1151b665eca0b37
Maddie Cusimano <maddiecusimano@Maddies-MacBook-Pro.local>
2020-10-20 12:33:08 -0400

    add vector mean

---
+++ src/means/mVec.jl
@@ -0,0 +1,48 @@
+#Constant mean function
+
+"""
+    MeanVec <: Mean
+
+Mean function for a 2D GP, 
+with constant mean along one dimension
+```math
+m(x) = [β_1, ..., β_n]
+```
+with constants ``β_1``, ..., ``β_n``.
+"""
+mutable struct MeanVec <: Mean
+    "Constant vector"
+    β::Array{Float64,1}
+    "x points corresponding to `β`"
+    x0::Array{Float64,1}
+    "Priors for mean parameters"
+    priors::Array
+    """
+        MeanVec(β::Array{Float64,1})
+
+    Create `MeanVec` with constant vector `β`.
+    """
+    MeanVec(β::Array{Float64,1}, x0::Array{Float64,1}) = new(β, x0, [])
+end
+
+mean(mVec::MeanVec, x::AbstractVector) = mVec.β[ [findfirst(isequal(_x[2]),mVec.x0) for _x in x] ]
+function mean(mVec::MeanVec, X::AbstractMatrix)
+    mean_vector = zeros(size(X,2))
+    for i in 1:size(X,2)
+        mean_vector[i] = mVec.β[findfirst(isequal(X[2,i]),mVec.x0)]
+    end
+    return mean_vector
+end
+
+get_params(mVec::MeanVec) = mVec.β
+get_param_names(::MeanVec) = [:β]
+num_params(mVec::MeanVec) = length(mVec.β)
+function set_params!(mVec::MeanVec, hyp::AbstractVector)
+    length(hyp) == 2 || throw(ArgumentError("Constant vector mean function has 1 parameter and 1 index."))
+    mVec.β = hyp[1]
+    mVec.x0 = hyp[2]
+end
+function grad_mean(mVec::MeanVec, x::AbstractVector)
+    dM_theta = ones(length(x))
+    return dM_theta
+end
